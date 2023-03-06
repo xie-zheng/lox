493b8220912cbf2110bda7ac756c64880e213f18
xiezheng <xiezheng.dev@outlook.com>
2023-03-06 14:42:24 +0800

    chapter5: challenges

---
+++ jlox/RPNPrinter.java
@@ -0,0 +1,62 @@
+package jlox;
+
+// exercise 5.3
+// reverse Polish notation
+class RPNPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme,
+                expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+//        builder.append("(");
+        for (Expr expr : exprs) {
+            builder.append(expr.accept(this));
+            builder.append(" ");
+        }
+        builder.append(name);
+//        builder.append(")");
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Binary(
+                        new Expr.Literal(1),
+                        new Token(TokenType.PLUS, "+", null, 1),
+                        new Expr.Literal(2)
+                ),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Binary(
+                        new Expr.Literal(4),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(3)
+                )
+        );
+
+        System.out.println(new RPNPrinter().print(expression));
+    }
+}
