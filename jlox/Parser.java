867bba7e41e0c37d183ad32033aade8d66a848d1
xiezheng <xiezheng.dev@outlook.com>
2023-11-29 10:08:49 +0800

    chapter10: support for function definition

--- jlox/Parser.java
+++ jlox/Parser.java
@@ -36,6 +36,9 @@ class Parser {
 
   private Stmt declaration() {
     try {
+      if (match(FUN)) {
+        return function("function");
+      }
       if (match(VAR)) {
         return varDeclaration();
       }
@@ -44,6 +47,28 @@ class Parser {
       synchronize();
       return null;
     }
+  }
+
+  private Stmt.Function function(String kind) {
+    Token name = consume(IDENTIFIER, "Expect " + kind + " name.");
+
+    consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
+    List<Token> parameters = new ArrayList<>();
+    if (!check(RIGHT_PAREN)) {
+      do {
+        if (parameters.size() >= 255) {
+          error(peek(), "Can't have more than 255 parameters.");
+        }
+
+        parameters.add(consume(IDENTIFIER, "Expect parameter name."));
+      } while (match(COMMA));
+    }
+    consume(RIGHT_PAREN, "Expect ')' after parameters");
+
+    consume(LEFT_BRACE, "Expect '{' after .");
+    List<Stmt> body = block();
+
+    return new Stmt.Function(name, parameters, body);
   }
 
   private Stmt varDeclaration() {
